c6eea5d59d5c8ba96317bd2dfb20be553089f809
Viral Solani <viral.solani@gmail.com>
2018-04-30 23:25:03 +0530

    Hero Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+//services
+import { HeroService } from './hero.service';
+
 
 
 @NgModule({
